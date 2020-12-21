d11302000520f2bd7c4a37d98da51bcf17229a7a
fish <fro-fish@ya.ru>
2020-12-21 23:21:10 +0300

    Config mysql connection

--- api/src/main/java/ru/corndev/api/domain/Playground.java
+++ api/src/main/java/ru/corndev/api/domain/Playground.java
@@ -1,4 +1,70 @@
 package ru.corndev.api.domain;
 
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
 public class Playground {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String playgroundName;
+
+    private String playgroundDescription;
+
+    private Long ownerId;
+
+    private Date createdDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdDate = new Date();
+    }
+
+    public Playground() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaygroundName() {
+        return playgroundName;
+    }
+
+    public void setPlaygroundName(String playgroundName) {
+        this.playgroundName = playgroundName;
+    }
+
+    public String getPlaygroundDescription() {
+        return playgroundDescription;
+    }
+
+    public void setPlaygroundDescription(String playgroundDescription) {
+        this.playgroundDescription = playgroundDescription;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 }
