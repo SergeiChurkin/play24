baf693f9405a9d3aea0b2a17b34bef9a6f33f724
FrozenFish <sergei.agni@gmail.com>
2020-12-23 16:15:45 +0300

    add frontend part of a project

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Play 24/7<br/>
+          Play everywhere
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
