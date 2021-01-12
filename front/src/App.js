1b6aae28c6ccdc934d265a0dbd91138c0b831140
FrozenFish <sergei.agni@gmail.com>
2021-01-12 15:26:26 +0300

    add redux store to frontend

--- front/src/App.js
+++ front/src/App.js
@@ -5,19 +5,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddEvent from "./components/Event/AddEvent";
 import Landing from "./components/Landing";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="container">
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addEvent" component={AddEvent} />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addEvent" component={AddEvent} />
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
