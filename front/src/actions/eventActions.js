568d5bcf3e1120bc7e85626833f05151d45157a3
Sergei <frozenfish2d@users.noreply.github.com>
2021-01-17 14:19:06 +0300

    fix eventActions.js format error

--- front/src/actions/eventActions.js
+++ front/src/actions/eventActions.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { GET_ERRORS } from "./types";
 
-export const createEvent = (eventItem, history) = async dispatch => {
-  try {
-    const res = await axios.post("http://localhost:8080/api/event", eventItem);
-    history.push("/dashboard");
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data,
-    });
+
+
+export const createEvent = (event, history) => async dispatch =>{
+  try{
+      const res = await axios.post("http://localhost:8080/api/event", event)
+      history.push("/")
+  }catch(err){
+      dispatch({
+          type:GET_ERRORS,
+          payload:err.response.data
+      })
   }
-};
+}
