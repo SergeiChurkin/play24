0e1aab972a8d94a109d226aee44420a782af955a
fish <fro-fish@ya.ru>
2021-04-01 00:33:07 +0300

    front/ prettier ShowEvent

--- front/src/components/Event/ShowEvent.js
+++ front/src/components/Event/ShowEvent.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import classnames from "classnames";
 import { getEvent, createEvent } from "../../actions/eventActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import axios from "axios";
 
 class ShowEvent extends Component {
   constructor() {
@@ -48,25 +46,62 @@ class ShowEvent extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getEvent(id, this.props.history);
-    axios.get("/api/eventtypes/all").then((res) => {
-      const types = res.data;
-      this.setState({ types });
-    });
   }
+
+  daySwitch(param) {
+    switch (param) {
+      case "1":
+        return "Понедельник";
+      case "2":
+        return "Вторник";
+      case "3":
+        return "Среда";
+      case "4":
+        return "Четверг";
+      case "5":
+        return "Пятница";
+      case "6":
+        return "Суббота";
+      case "7":
+        return "Воскресенье";
+      default:
+        return "";
+    }
+  }
+
   render() {
     const { errors } = this.state;
     return (
       <div className="container">
-        {this.state.eventName}
-        <br />
-        {this.state.eventType.typeName}
-        <br />
-        {this.state.eventDate}
-        {this.state.schedules.map((item) => (
-          <p key={item.id}>
-            {item.day} {item.time}
-          </p>
-        ))}
+      <div className="card border-info mb-3  text-center">
+      <h2 className="card-header">{this.state.eventName}</h2>
+      </div>
+        <div className="row">
+
+          <div className="col-6">
+            <div className="card border-info mb-3">
+              <h4 className="card-header">{this.state.eventType.typeName}</h4>
+              <div className="card-body">
+                {this.state.eventDate && (
+                  <p className="card-text">{this.state.eventDate}</p>
+                )}
+
+                {this.state.schedules.map((item) => (
+                  <p className="card-text" key={item.id}>
+                    {this.daySwitch(item.day)}
+                    {item.time}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="card border-info mb-3">
+              <h4 className="card-header">Участники</h4>
+              <div className="card-body"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
